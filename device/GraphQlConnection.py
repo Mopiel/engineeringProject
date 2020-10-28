acd681cf177933c082cc1d89938c9e79f1c8bb67
plbakol2 <bartek.kolodziejczyk.bk@gmail.com>
2020-10-28 18:07:07 +0100

    added ip

--- device/GraphQlConnection.py
+++ device/GraphQlConnection.py
@@ -3,16 +3,16 @@ import json
 from datetime import datetime
 from uuid import getnode as get_mac
 
-url = 'http://localhost:4000/graphql'
+url = 'http://192.168.1.14:4000/graphql'
+mac_hex = "{:012x}".format(get_mac())
+device = ":".join(mac_hex[i:i+2] for i in range(0, len(mac_hex), 2))
 
 
 def sendData(name, rssi):
-    mac_hex = "{:012x}".format(get_mac())
-    device = ":".join(mac_hex[i:i+2] for i in range(0, len(mac_hex), 2))
 
     query = 'mutation { updateBeacon( name: "'+name+'", device: "' + \
         device+'", rssi: ' + str(rssi) + ') { id } }'
-    
+
     json = {'query':  query}
 
     r = requests.post(url=url, json=json)
@@ -20,6 +20,5 @@ def sendData(name, rssi):
     if(r.status_code == 200):
         return True
     return False
-
 
 print(sendData("12:31:12:42:21:35:52", 42))
