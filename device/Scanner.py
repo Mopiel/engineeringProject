8a9350b8aa3962827fbe5abd069795c4f7796310
plbakol2 <bartek.kolodziejczyk.bk@gmail.com>
2020-10-28 18:35:20 +0100

    added ip

--- device/Scanner.py
+++ device/Scanner.py
@@ -1,11 +1,20 @@
-
 import time
+
 from beacontools import BeaconScanner, EddystoneTLMFrame, EddystoneFilter
 
 
+# scan for all TLM frames of beacons in the namespace "12345678901234678901"
+# scanner = BeaconScanner(callback,
+#     # remove the following line to see packets from all beacons
+#     #device_filter=EddystoneFilter(namespace="11111111-1111-1111-1111-111111111111"),
+#     #packet_filter=EddystoneTLMFrame
+# )
+# scanner.start()
+# time.sleep(10)
+# scanner.stop()
+
 def scan(scanTime):
     scanArray = []
-
     def callback(bt_addr, rssi, packet, additional_info):
         if(additional_info and additional_info["uuid"] == "11111111-1111-1111-1111-111111111111"):
             added = False
@@ -15,23 +24,19 @@ def scan(scanTime):
                     added = True
                     break
             if(added == False):
-                scanArray.append({"addr": bt_addr, "rssi": [
-                                 rssi], "uuid": additional_info["uuid"], "major": additional_info["major"]})
+                scanArray.append({"addr":bt_addr, "rssi":[rssi], "uuid":additional_info["uuid"],"major":additional_info["major"]})
 
     scanner = BeaconScanner(callback)
     scanner.start()
     time.sleep(scanTime)
     scanner.stop()
     newArray = []
-    print(scan)
     for scan in scanArray:
-        newArray.append({"addr": scan["addr"], "rssi": sum(
-            scan["rssi"])/len(scan["rssi"]), "uuid": scan["uuid"], "major": scan["major"]})
+        newArray.append({"addr":scan["addr"], "rssi":sum(scan["rssi"])/len(scan["rssi"]), "uuid":scan["uuid"],"major":scan["major"]})
     return newArray
 
 
 for x in range(20):
-    print("Number 1")
     for n in scan(1):
         print(n)
         print(10**((-69-n["rssi"])/(10**2)))
