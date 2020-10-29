72bc7d06ea2db475c82612392ee1c5b7480f161d
plbakol2 <bartek.kolodziejczyk.bk@gmail.com>
2020-10-29 19:01:42 +0100

    added new Scanner

--- device/Scanner.js
+++ device/Scanner.js
@@ -4,7 +4,11 @@ const BeaconScanner = require("node-beacon-scanner");
 var scanner = new BeaconScanner();
 
 scanner.onadvertisement = (advertisement) => {
-  console.log(advertisement["iBeacon"], advertisement["rssi"]);
+//   if (
+//     advertisement &&
+//     advertisement.iBeacone.uuid === "11111111-1111-1111-1111-111111111111"
+//   )
+    console.log(advertisement);
 };
 
 scanner
@@ -15,3 +19,18 @@ scanner
   .catch((error) => {
     console.error(error);
   });
+
+const noble = require("noble");
+
+noble.startScanning();
+
+// noble.on("discover", function (peripheral) {
+//   var macAddress = peripheral.uuid;
+//   var rss = peripheral.rssi;
+//   var localName = "jakies";
+//   console.log(peripheral.advertisement);
+// });
+
+// noble.on("stateChange", () => {
+//   console.log("changed");
+// });
