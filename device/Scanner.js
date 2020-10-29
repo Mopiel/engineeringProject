c3733bf815dcde73a846c43077d69f7688b417ad
plbakol2 <bartek.kolodziejczyk.bk@gmail.com>
2020-10-29 17:22:12 +0100

    fixed Scanner

--- device/Scanner.js
+++ device/Scanner.js
@@ -4,13 +4,14 @@ const BeaconScanner = require("node-beacon-scanner");
 var scanner = new BeaconScanner();
 
 scanner.onadvertisement = (advertisement) => {
-    var beacon = advertisement["iBeacon"];
-    beacon.rssi = advertisement["rssi"];
-    console.log(JSON.stringify(beacon, null, "    "))
+  console.log(advertisement["iBeacon"], advertisement["rssi"]);
 };
 
-scanner.startScan().then(() => {
-    console.log("Scanning for BLE devices...")  ;
-}).catch((error) => {
+scanner
+  .startScan()
+  .then(() => {
+    console.log("Scanning for BLE devices...");
+  })
+  .catch((error) => {
     console.error(error);
-});
+  });
