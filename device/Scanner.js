c9fe49643baadcf414867c9bb9b83e25ff7c62b3
plbakol2 <bartek.kolodziejczyk.bk@gmail.com>
2020-11-04 18:17:01 +0100

    added full configuration

--- device/Scanner.js
+++ device/Scanner.js
@@ -1,29 +1,30 @@
 const Noble = require("noble");
 const BeaconScanner = require("node-beacon-scanner");
 const fetch = require("node-fetch");
-const address = require('address');
-console.log(address.ip())
+const address = require("address");
+
+const device = address.ip();
 
 url = "http://192.168.1.14:4000/graphql";
 
-// var scanner = new BeaconScanner();
+var scanner = new BeaconScanner();
 
-// scanner.onadvertisement = (advertisement) => {
-//   if (advertisement.iBeacon.uuid !== "11111111-1111-1111-1111-111111111111")
-//     return;
-//   const {id} = advertisement
-//   console.log(advertisement);
-//   sendData(id,)
-// };
+scanner.onadvertisement = (advertisement) => {
+  if (advertisement.iBeacon.uuid !== "11111111-1111-1111-1111-111111111111")
+    return;
+  const { id, rssi, iBeacon } = advertisement;
+  const { txPower } = iBeacon;
+  console.log(sendData(id, device, rssi, txPower));
+};
 
-// scanner
-//   .startScan()
-//   .then(() => {
-//     console.log("Scanning for BLE devices...");
-//   })
-//   .catch((error) => {
-//     console.error(error);
-//   });
+scanner
+  .startScan()
+  .then(() => {
+    console.log("Scanning for BLE devices...");
+  })
+  .catch((error) => {
+    console.error(error);
+  });
 
 const sendData = (name, device, rssi, txpower) => {
   query = `mutation { updateBeacon( name: "${name}", device: "${device}", rssi: ${rssi},txpower: ${txpower}) { id } }`;
@@ -38,4 +39,3 @@ const sendData = (name, device, rssi, txpower) => {
     .then((r) => r.json())
     .then((data) => console.log("data returned:", data));
 };
-
